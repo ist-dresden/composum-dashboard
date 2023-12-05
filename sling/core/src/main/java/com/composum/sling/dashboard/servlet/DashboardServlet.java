6a55f1c41e58542bf7647f207a9a97a52ebb444c
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-12-05 17:15:23 +0100

    fix exception on broken configuration in DashboardServlet

--- sling/core/src/main/java/com/composum/sling/dashboard/servlet/DashboardServlet.java
+++ sling/core/src/main/java/com/composum/sling/dashboard/servlet/DashboardServlet.java
@@ -31,6 +31,8 @@ import org.osgi.service.component.annotations.ReferencePolicyOption;
 import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.Designate;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
@@ -61,6 +63,8 @@ import static com.composum.sling.dashboard.servlet.AbstractWidgetServlet.OPTION_
 )
 @Designate(ocd = DashboardServlet.Config.class)
 public class DashboardServlet extends AbstractDashboardServlet implements DashboardPlugin, ContentGenerator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DashboardServlet.class);
 
     public static final String DASHBOARD_CONTEXT = "dashboard";
 
@@ -286,18 +290,22 @@ public class DashboardServlet extends AbstractDashboardServlet implements Dashbo
         } else {
             for (final String item : this.navigation) {
                 final Matcher matcher = NAVIGATION_PATTERN.matcher(item);
-                final String linkUrl = matcher.group("link");
-                if (StringUtils.isNotBlank(linkUrl)) {
-                    final String name = matcher.group("name");
-                    if (matcher.matches()) {
-                        final String label = StringUtils.defaultString(matcher.group("label"), name);
-                        writer.append("<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\" data-href=\"")
-                                .append(linkUrl).append("\"");
-                        if (StringUtils.isNotBlank(label)) {
-                            writer.append(" title =\"").append(label).append("\"");
+                if (matcher.matches()) {
+                    final String linkUrl = matcher.group("link");
+                    if (StringUtils.isNotBlank(linkUrl)) {
+                        final String name = matcher.group("name");
+                        if (matcher.matches()) {
+                            final String label = StringUtils.defaultString(matcher.group("label"), name);
+                            writer.append("<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\" data-href=\"")
+                                    .append(linkUrl).append("\"");
+                            if (StringUtils.isNotBlank(label)) {
+                                writer.append(" title =\"").append(label).append("\"");
+                            }
+                            writer.append(">").append(label).append("</a></li>");
                         }
-                        writer.append(">").append(label).append("</a></li>");
                     }
+                } else {
+                    LOG.debug("invalid navigation item: '{}'", item);
                 }
             }
         }
